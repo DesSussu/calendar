652fcf2815b49232a7b2d963921fcf0c52606abe
Desi M <desiremrv@gmail.com>
2026-01-13 20:15:07 +0100

    Add environment files for deployment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBFa9zYWqQAWhMNHlpc32lfh3KY1nJcK9Y",
+    authDomain: "calendar-ec999.firebaseapp.com",
+    databaseURL: "https://calendar-ec999-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "calendar-ec999",
+    storageBucket: "calendar-ec999.firebasestorage.app",
+    messagingSenderId: "7004657186",
+    appId: "1:7004657186:web:b280e0a775513fa8115c7b",
+    measurementId: "G-N8LB7SDD7E"
+  },
+  supabase: {
+    url: "https://mnlfulqxkuwykskjdugg.supabase.co",
+    anonKey: "sb_publishable__V_li6N32CaQQaV89J_SJg_58lHV7kN"
+  }
+};
